c29808b5c4a332f220dba66593889990a4153b38
Haydn Tamura <haydnt@zohomail.com>
2025-06-25 10:53:40 -0700

    better cluster struct and removed old declarations

--- include/kmeans.h
+++ include/kmeans.h
@@ -7,25 +7,24 @@
 #include <stddef.h>
 typedef struct {
 
-    Pixel pixel;
-    int clusterId;
+  Pixel pixel;
 } Point;
 
 typedef struct {
-    Pixel   centroid;       // centroid value
-    double rgbCumm[3];
-    int     clusterId;
-    size_t     nPoints;
-    size_t _arrSize;
+  Pixel centroid; // centroid value
+  double rgbCumm[3];
+  size_t nPoints;
 } Cluster;
 
-int initializeClusters(Cluster** clusters, Point** points, Point** chosenCentroids, int numClusters, int numPoints);
-int calculateNewMean(Cluster* cluster);
-int clusterAddPoint(Cluster* cluster, const Point* point);
-void clusterResetPoints(Cluster* cluster);
-void firstStep(Cluster** clusters, Point** points, Point** chosenCentroids, int numClusters, int numPoints, int nThreads);
-void refineClusters(Cluster** clusters, Point** points, int numClusters, int numPoints, int nThreads);
+int initializeClusters(Cluster **clusters, Point **points, int numClusters,
+                       int numPoints);
+int calculateNewMean(Cluster *cluster);
+int clusterAddPoint(Cluster *cluster, const Point *point);
+void clusterResetPoints(Cluster *cluster);
+void refineClusters(Cluster **clusters, Point **points, int numClusters,
+                    int numPoints, int nThreads);
 
-Pixel* generatePalette(Point** points, int numPoints, int numClusters, int numThreads);
+Pixel *generatePalette(Point **points, int numPoints, int numClusters,
+                       int numThreads);
 
-#endif //KMEANS_H
+#endif // KMEANS_H
